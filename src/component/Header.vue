f98f6dbb8c0e65792d972d6239a92831e9b5574e
justgotlakisean <joshua222.98@gmail.com>
2026-03-16 07:03:56 +0800

    created header component

---
+++ src/component/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+// const header = ref("The header")
+</script>
+
+<template>
+    <header>
+        <h1>Qoute Generator</h1>
+    </header>
+</template>
+
+<style scoped>
+header {
+    color: #CCD6D9;
+    margin-bottom: 25px;
+}
+</style>
